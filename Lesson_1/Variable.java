4c0df8b3b23e42cbae495b1716f77e5a86d9b02e
Glavegor <glavnegor@yandex.ru>
2020-12-21 13:24:52 +0500

    add Variable

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {	
+    public static void main(String[] args) {
+      
+      byte ram = 16;
+      short ssd = 256;
+      int putinAge = 68;
+      long costOfEducation = 1000000L;
+      float piNumber = 3.1454345F;
+      double eNumber = 2.718;
+      char myname = 'E';
+      boolean homeopathy = false;
+
+      System.out.println(ram);
+      System.out.println(ssd);
+      System.out.println(putinAge);
+      System.out.println(costOfEducation);
+      System.out.println(piNumber);
+      System.out.println(eNumber);
+      System.out.println(myname);
+      System.out.println(homeopathy);
+      
+
+    }
+}
